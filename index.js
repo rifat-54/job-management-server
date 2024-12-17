91d6ab9b91067516474e2150e9067f9abe26bcf6
rifat-54 <rifat999por@gmail.com>
2024-12-17 18:50:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+require('dotenv').config()
+
+const port = process.env.PORT || 9000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+// const uri="mongodb://localhost:27017"
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umkvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+
+    const db=client.db('job-management');
+    const jobCollection=db.collection('jobs')
+
+    
+    app.post('/add-job',async (req,res)=>{
+      const jobData=req.body;
+      
+      const result=await jobCollection.insertOne(jobData)
+      res.send(result)
+    })
+
+
+    app.get('/jobs',async(req,res)=>{
+      const result=await jobCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/jobs/:email',async(req,res)=>{
+      const email=req.params.email;
+      const query={'buyer.eamil':email}
+      const result=await jobCollection.find().toArray(query)
+      res.send(result)
+    })
+
+    // delete a job
+
+    app.delete('/job/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result= await jobCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // get a specic data
+
+    app.get('/job/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result= await jobCollection.findOne(query)
+      res.send(result)
+    })
+
+    // update a job
+
+    app.put('/update-job/:id',async(req,res)=>{
+      const id=req.params.id;
+      const updateData=req.body;
+      const filter={_id: new ObjectId(id)}
+      const options={upsert:true}
+      const updateDoc={
+        $set:updateData
+      }
+      const result= await jobCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })
+
+
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send('Hello from SoloSphere Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
